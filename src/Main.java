8f0ab0af22d2dbb23078c6ae9d73e104561aedc1
ulyana <bsergeybabak2000@gmail.com>
2023-03-15 15:11:25 +0300

    последний коммит

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
+        task6();
     }
 
     public static void task1() {
@@ -33,15 +35,30 @@ public class Main {
             System.out.print(n2[i] + ", ");
         }
     }
-    public static void task4(){
+
+    public static void task4() {
         int[] n3 = {90, 91, 93, 92, 85, 87, 84, 83, 0, 0, 0, 0};
-        for(int i = 0; i < n3.length; i++){
-            if (i == n3.length - 1){
+        for (int i = 0; i < n3.length; i++) {
+            if (i == n3.length - 1) {
                 System.out.println(n3[i]);
                 break;
             }
             System.out.print(n3[i] + ", ");
         }
+    }
+
+    public static void task5() {
+        int[] n1 = {1, 2, 3};
+        for(int i = n1.length - 1; i >= 0; i--){
+            if (i == 0){
+                System.out.println(n1[i]);
+                break;
+            }
+            System.out.print(n1[i] + ", ");
+        }
+    }
+    public static void task6(){
+
     }
 }
 
